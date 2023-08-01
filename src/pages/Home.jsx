312c5e330a89afa0d0dcd27f159a3c43738963d6
olafpd <olafpd@umich.edu>
2023-08-01 18:31:07 -0400

    undid merge

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+function Home() {
+    return (
+      <section class="page-404">
+        <div class="bubble-lg" />
+        <div class="bubble-sm" />
+        <h1 class="type-heading-1">
+          This is the home page
+        </h1>
+      </section>
+    );
+  }
+  
+  export default Home;
