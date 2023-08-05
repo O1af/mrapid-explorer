692f93206bab98039fffad0d68707d2bb1033a87
Ameli4 <framelia@umich.edu>
2023-08-05 17:37:05 -0400

    added AQI/conc toggle (front end only)

--- src/components/MapCards/Accordion.jsx
+++ src/components/MapCards/Accordion.jsx
@@ -6,6 +6,7 @@ import {
 } from 'solid-js';
 import { createStore } from 'solid-js/store';
 import { useStore } from '../../stores';
+import {Radio, RadioGroup} from "solid-blocks";
 
 const AccordionContext = createContext();
 
@@ -107,125 +108,10 @@ function AccordionPanel(props) {
   );
 }
 
-// function ThresholdSelect() {
-//   const [thresholdParameter, setThresholdParameter] = createSignal(2); // initialize with PM25
-//   const [thresholdValue, setThresholdValue] = createSignal(5); // initialize at 5ugm3
-//   const [thressholdPeriod, setThressholdPeriod] = createSignal(14); // initialize at 14 days
-
-//   const [{ setMapThreshold }] = useStore();
-
-//   function setThreshold() {
-//     setMapThreshold({
-//       active: true,
-//       parameter_id: thresholdParameter(),
-//       threshold: thresholdValue(),
-//       period: thressholdPeriod(),
-//     });
-//   }
-
-//   const parameters = [
-//     { value: 2, display: 'PM2.5' },
-//     { value: 1, display: 'PM10' },
-//     { value: 3, display: 'O₃' },
-//     { value: 5, display: 'NO₂' },
-//   ];
-//   const periods = [
-//     { value: 14, display: 'Last 14 days' },
-//     { value: 30, display: 'Last 30 days' },
-//     { value: 90, display: 'Last 90 days' },
-//   ];
-//   const thresholdValues = [
-//     { value: 5, display: 5, parameter: 2 },
-//     { value: 10, display: 10, parameter: 2 },
-//     { value: 250, display: 250, parameter: 2 },
-//     { value: 15, display: 15, parameter: 1 },
-//     { value: 20, display: 20, parameter: 1 },
-//     { value: 100, display: 100, parameter: 3 },
-//     { value: 10, display: 10, parameter: 5 },
-//     { value: 40, display: 40, parameter: 5 },
-//   ];
-
-//   const [thresholds, setThresholds] = createSignal(
-//     thresholdValues.filter((o) => o.parameter == 2)
-//   );
-
-//   function onParameterChange(e) {
-//     setThresholds(
-//       thresholdValues.filter((o) => o.parameter == e.target.value)
-//     );
-//     setThresholdValue(
-//       thresholdValues.filter((o) => o.parameter == e.target.value)[0]
-//         .value
-//     );
-//     setThresholdParameter(parseInt(e.target.value));
-//   }
-
-//   return (
-//     <>
-//       <select
-//         name=""
-//         id=""
-//         class="select"
-//         onChange={onParameterChange}
-//       >
-//         <For each={parameters}>
-//           {(parameter) => (
-//             <option value={parameter.value}>
-//               {parameter.display}
-//             </option>
-//           )}
-//         </For>
-//       </select>
-//       <div class="thresholds-controls">
-//         <span class="thresholds-controls__item">Above</span>
-//         <select
-//           name=""
-//           id=""
-//           class="select thresholds-controls__item"
-//           onChange={(e) =>
-//             setThresholdValue(parseInt(e.target.value))
-//           }
-//         >
-//           <For
-//             each={thresholds().filter(
-//               (o) => o.parameter === thresholdParameter()
-//             )}
-//           >
-//             {(threshold, i) => (
-//               <option value={threshold.value} selected={i === 1}>
-//                 {threshold.display}
-//               </option>
-//             )}
-//           </For>
-//         </select>
-//         <span class="thresholds-controls__item">µg/m³</span>
-//       </div>
-//       <select
-//         name=""
-//         id=""
-//         class="select"
-//         onChange={(e) =>
-//           setThressholdPeriod(parseInt(e.target.value))
-//         }
-//       >
-//         <For each={periods}>
-//           {(period, i) => (
-//             <option value={period.value} selected={i == 0}>
-//               {period.display}
-//             </option>
-//           )}
-//         </For>
-//       </select>
-//       <button class="btn btn-secondary" onClick={setThreshold}>
-//         Update
-//       </button>
-//     </>
-//   );
-// }
-
 export default function Accordion() {
   const [store, { loadParameter }] =
     useStore();
+  
 
   //on load get aqi value from openweathermap api and store
   // const [aqi, setAqi] = createSignal(0); 
@@ -242,33 +128,11 @@ export default function Accordion() {
   //   console.log(data);
   //   setAqi(aqi1);
   // });
+
+  
   
   return (
     <AccordionProvider>
-      {/* <AccordionPanel
-        name="aqi"
-        title="Air Quality Index"
-        contentKey="aqi"
-        active={!store.mapThreshold.active}
-        open={true}
-      >
-        <meter
-          class="aqi-bar"
-          min="0"
-          max="5"
-          value={aqi()}
-        />
-        <meter
-          class="aqi-arrow"
-          min="0"
-          max="5"
-          value={aqi()}
-        />
-        <p>AQI: {aqi()}</p>
-        <p>1 = Good, 2 = Fair, 3 = Moderate, 4 = Poor, 5 = Very Poor.
-        </p>
-
-      </AccordionPanel> */}
       <AccordionPanel
         name="pollutants"
         title="Pollutant"
@@ -276,6 +140,11 @@ export default function Accordion() {
         active={!store.mapThreshold.active}
         open={true}
       >
+        <h1>Select the unit and pollutant</h1>
+        <RadioGroup>
+        <Radio>AQI</Radio>
+        <Radio checked={true}>Concentration</Radio>
+        </RadioGroup>
         <select
           name=""
           id=""
