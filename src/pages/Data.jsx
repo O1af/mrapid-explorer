81eb4fec594d5fa96402485f9a51c2d9d0ddb8ef
jennyx <jennyx@umich.edu>
2023-08-08 18:51:03 -0400

    changed dropdowns

--- src/pages/Data.jsx
+++ src/pages/Data.jsx
@@ -2,42 +2,13 @@ import { createSignal, createUniqueId } from "solid-js";
 import { Select, createOptions } from "@thisbeyond/solid-select";
 import "@thisbeyond/solid-select/style.css";
 import './search.scss';
+import { MultiSelect } from '@digichanges/solid-multiselect';
 
 const Data = () => {
-
   const createValue = (name) => {
     return { id: createUniqueId(), name };
   };
-
-  const candidates = [
-    createValue("DST : 101 2236 14TH STREET (95120)"),
-    createValue("OAQ : PORT HURON (48060)"),
-    createValue("PAR : Appoline St (48227)"),
-    createValue("CLA : AHKQKKTX (48211)"),
-    createValue("TSI : FBPOWER1 (58757)"),
-  ];
-
-  const initialValue = [];
-
-  const [options, setOptions] = createSignal(candidates);
-  const [selectedValues, setSelectedValues] = createSignal(initialValue);
-
-  const onChange = (selected) => {
-    setSelectedValues(selected);
-
-    const lastValue = selected[selected.length - 1];
-    if (lastValue && !options().includes(lastValue)) {
-      setOptions([...options(), lastValue]);
-    }
-  };
-
-  const props = createOptions(options, {
-    key: "name",
-    disable: (value) => selectedValues().includes(value),
-    filterable: true, // Default
-    createable: createValue,
-  });
-
+  
     return (
       <section class="page-data">
         
@@ -47,27 +18,40 @@ const Data = () => {
           Download sensor data
         </h1>
 
-        <div class="flex flex-1 flex-col max-w-100 gap-3">
-          Select sensor(s):
-          <Select
-            class="search"
-            multiple
-            label="Select sensors"
-            placeholder="Search by monitor type, sensor name, or zip code..."
-            onChange={onChange}
-            {...props}
-          />
-        </div>
         
-          <label class="data-form-item">
-            Pollutant
-            <select class="select">
-              <option>PM 2.5</option>
-              <option>PM 10</option>
-              <option>O3</option>
-              <option>CO</option>
-            </select>
-          </label>
+        <label class="data-form-item">
+        Zip Code(s)
+          <MultiSelect
+            style={{ chips: { color: "purple", "background-color": "pink" } }}
+            options={["95120", "48060", "48227", "48211", "58757"]}
+            onSelect={console.log}
+            onRemove={console.log}
+            class="search"
+            //selectedValues={["yellow"]}
+            //selectionLimit={2}
+          />
+        </label>
+
+        <label class="data-form-item">
+        Monitor Type(s)
+          <MultiSelect
+            style={{ chips: { color: "purple", "background-color": "pink" } }}
+            options={["DST", "OAQ", "PAR", "CLA", "TSI"]}
+            onSelect={console.log}
+            onRemove={console.log}
+            class="search"
+          />
+        </label>
+        <label class="data-form-item">
+        Pollutant(s)
+          <MultiSelect
+            style={{ chips: { color: "purple", "background-color": "pink" } }}
+            options={["PM 2.5", "PM 10", "O3", "CO"]}
+            onSelect={console.log}
+            onRemove={console.log}
+            class="search"
+          />
+        </label>
           <label class="data-form-item">
             Start time/date
             <input type="datetime-local" name="start-time" class="text-input"></input>
