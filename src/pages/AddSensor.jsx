788a6b964d66522971562055a9fb1164bc2481e4
jennyx <jennyx@umich.edu>
2023-09-11 23:15:13 -0400

    working sensor fetch

--- src/pages/AddSensor.jsx
+++ src/pages/AddSensor.jsx
@@ -1,7 +1,10 @@
-import { createSignal, createResource, For, Show } from "solid-js";
+import { createSignal, createResource, For, Show, createUniqueId} from "solid-js";
 import { searchSensors } from "./searchSensors";
 import './search.scss';
 import { MultiSelect } from '@digichanges/solid-multiselect';
+import { Select, createSelect, createOptions } from "@thisbeyond/solid-select";
+import "@thisbeyond/solid-select/style.css";
+import './search.scss';
 
 const filters = { zip_code: "", type: "", pollutant: "" };
 
@@ -13,10 +16,34 @@ export function AddSensor(props) {
   const [showForm, setShowForm] = createSignal(false);
   const toggleForm = () => setShowForm(!showForm());
 
+  const createValue = (name) => {
+    return { id: createUniqueId(), name };
+  };
+
+  const sensors = [
+    createValue("DST : 101 2236 14TH STREET (95120)"),
+    createValue("DST : 102 TRINITY (20147)"),
+  ];
+  const initialSensors = [];
+  const [sensorOptions, sensorSetOptions] = createSignal(sensors);
+  const [sensorSelectedValues, sensorSetSelectedValues] = createSignal(initialSensors);
+  const onChangeSensor = (selected) => {
+    sensorSetSelectedValues(selected);
+
+    //setInput({ ...input(), q: selected});
+    //input().q = selectedValues;
+  };
+  const propsSensor = createOptions(sensorOptions, {
+    key: "name",
+    disable: (value) => sensorSelectedValues().includes(value),
+    filterable: true, // Default
+    createable: createValue,
+  });
+
   return (
     <>
       <form>
-      <h3>Select Sensors</h3>
+      <h3>Find Sensors</h3>
 
         <div>
           <label for="zip_code">Filter sensors by zip code</label>
@@ -64,30 +91,32 @@ export function AddSensor(props) {
             }
         >
             <Show when={!data.loading} fallback={<>Searching...</>}>
-            
-            <label class="data-form-item">
-                Select sensors
-                <MultiSelect
-                    style={{ chips: { color: "purple", "background-color": "pink" } }}
-                    options={data()}
-                    onSelect={console.log}
-                    onRemove={console.log}
-                    class="search"
-                    //selectedValues={["yellow"]}
-                    //selectionLimit={2}
-                />
-            </label>
-            <ul>
-            <For each={data()}>   
-                {(sensor) => (
-                <li>
-                    {sensor.name}
-                </li>
-                )}
-            </For>
-            </ul>
+              <ul>
+              <For each={data().SensorList}>   
+                  {(sensor) => (
+                  <li>
+                      {sensor.name}
+                  </li>
+                  )}
+              </For>
+              </ul>
         </Show>
       </Show>
+
+      <h3>Filter Data</h3>
+      <div class="flex flex-1 flex-col max-w-100 gap-3">
+        <label for="sensor">Filter data by sensor</label>
+        <Select
+          class="search"
+          id="sensor"
+          //value={input().zip_code}
+          multiple
+          label="Select sensors"
+          placeholder="Search by sensor"
+          onChange={onChangeSensor}
+          {...propsSensor}
+        />
+      </div>
     </>
   );
 }
