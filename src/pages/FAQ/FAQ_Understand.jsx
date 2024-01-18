8fc55b8e51b10f1ed3d68378db1e95d9562951f5
Ameli4 <framelia@umich.edu>
2024-01-18 09:57:07 -0500

    quick front end fixes

--- src/pages/FAQ/FAQ_Understand.jsx
+++ src/pages/FAQ/FAQ_Understand.jsx
@@ -1,4 +1,4 @@
-import {PDFViewer} from "../../components/PDFViewer";
+
 
 function FAQ_UND() {
     return (
@@ -9,10 +9,7 @@ function FAQ_UND() {
                 <li><a href='/faq/goals'>Project Goals</a></li>
                 <li><a href='/faq/aq'>Air Quality Science</a></li>
             </ul>
-            <div>
-                <h2>PDF Viewer</h2>
-                <PDFViewer pdfUrl={"https://caphedetroit.sph.umich.edu/wp-content/uploads/2016/10/Resource-Manual-3.0-Air-Quality-Health-Environmental-Justice-Website-Version-10-4-16.pdf"} />
-            </div>
+            
 
         </section>
     
