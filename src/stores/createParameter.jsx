39768e4b840361330e0b65940efcae761cdcaea5
olafpd <olafpd@umich.edu>
2024-05-14 11:12:04 -0400

    updated initial param

--- src/stores/createParameter.jsx
+++ src/stores/createParameter.jsx
@@ -1,19 +1,17 @@
-import { createStore } from 'solid-js/store';
+import { createStore } from "solid-js/store";
 
 export default function createParameter(client, actions, state) {
   //const [parameterId, setParameter] = createSignal(2);
   const [parameter, setParameter] = createStore({
-    id: 1, //changes which pollutant initially appears on load
-    parameterName: 'Black C', //starting label for starting pollutant
-    unit: 'µg/m³',
+    id: 14, //changes which pollutant initially appears on load
+    parameterName: "PM 2.5", //starting label for starting pollutant
+    unit: "µg/m³",
   });
 
   Object.assign(actions, {
     loadParameter(id) {
       console.log(state.parameters());
-      const parameter = state
-        .parameters()
-        .find((x) => x.id === parseInt(id));
+      const parameter = state.parameters().find((x) => x.id === parseInt(id));
       setParameter({
         id: id,
         parameterName: parameter.displayName,
