a2110a040cd6827d29f2d8b22b2f255bc34ac9e4
olafpd <olafpd@umich.edu>
2024-06-03 22:24:14 -0400

    Updates

--- assets/PollutantHelp.7c876c11.js
+++ assets/PollutantHelp.7c876c11.js
@@ -1,4 +1,4 @@
-import{H as n}from"./HelpContent.0843187b.js";import{c as l}from"./index.ba4848f8.js";const e=`<h1>Pollutants</h1>
+import{H as n}from"./HelpContent.98396ca5.js";import{c as l}from"./index.0faa84d1.js";const e=`<h1>Pollutants</h1>
 <p>The Detroit Air database currently has data sensor data for the following pollutants. Note that we calculate the AQI values shown.</p>
 <h2>Criteria Pollutants:</h2>
 <ul>
