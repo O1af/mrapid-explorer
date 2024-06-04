aa5c1e62afaba5ca89dbe095f0f9cdd869532946
olafpd <olafpd@umich.edu>
2024-06-03 23:29:15 -0400

    Updates

--- assets/FilterHelp.1c04749e.js
+++ assets/FilterHelp.1c04749e.js
@@ -1,3 +1,3 @@
-import{H as e}from"./HelpContent.1189d3c5.js";import{c as t}from"./index.e3d906d7.js";const o=`<h1>Filters</h1>
+import{H as e}from"./HelpContent.e80c9aa0.js";import{c as t}from"./index.6f42b707.js";const o=`<h1>Filters</h1>
 <p>Detroit Air recieves data from sensors originating from multiple organiations, including the Environmental Protection Agency (EPA). Use the checkboxes to filter out the desired sensor sources. The map shows by default sensors that only measure the specified pollutant. To see all sensors, uncheck the 'Exclude inactive monitors' box.</p>
 `;function r(){return t(e,{html:o})}export{r as default};
--- assets/PollutantHelp.ec7b972f.js
+++ assets/PollutantHelp.ec7b972f.js
@@ -1,4 +1,4 @@
-import{H as n}from"./HelpContent.1189d3c5.js";import{c as l}from"./index.e3d906d7.js";const e=`<h1>Pollutants</h1>
+import{H as n}from"./HelpContent.e80c9aa0.js";import{c as l}from"./index.6f42b707.js";const e=`<h1>Pollutants</h1>
 <p>The Detroit Air database currently has data sensor data for the following pollutants. Note that we calculate the AQI values shown.</p>
 <h2>Criteria Pollutants:</h2>
 <ul>
